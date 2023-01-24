d9346d9e71971119345a43023a81c891ccca7174
Salma El-Akhal <salma.elakhal.pro@gmail.com>
2023-01-24 17:07:24 +0100

    more about Databinding

--- Components-Databinding-more/src/app/app.module.ts
+++ Components-Databinding-more/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventComponent } from './event/event.component';
+import { OddComponent } from './odd/odd.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventComponent,
+    OddComponent,
+    GameControlComponent
   ],
   imports: [
     BrowserModule
