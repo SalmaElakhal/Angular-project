c2b0723cd6299a0fe1b9df9d6399cc190bdd33ee
Salma El-Akhal <salma.elakhal.pro@gmail.com>
2023-01-25 00:09:52 +0100

    learn about directive

--- Components-Databinding-more/src/app/app.module.ts
+++ Components-Databinding-more/src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
-import { EventComponent } from './event/event.component';
-import { OddComponent } from './odd/odd.component';
 import { GameControlComponent } from './game-control/game-control.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventComponent,
+    GameControlComponent,
     OddComponent,
-    GameControlComponent
+    EvenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Components-Databinding-more/src/app/even/even.component.ts
+++ Components-Databinding-more/src/app/even/even.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-even',
-  templateUrl: './event.component.html',
-  styleUrls: ['./event.component.css']
+  templateUrl: './even.component.html',
+  styleUrls: ['./even.component.css']
 })
 export class EvenComponent implements OnInit {
   @Input() number: number;
